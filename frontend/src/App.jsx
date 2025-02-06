d6ba07e3743d7988b733ce428d66b23825ef136e
omjeejaiswal <sureshjaiswal808@gmail.com>
2025-02-06 22:53:05 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,63 +1,9 @@
-
-
-// import React, { useState, useEffect } from "react";
-// import { motion, AnimatePresence } from "framer-motion";
-// import Navbar from "../src/components/navbar";
-// import PhotoSlider from "./components/slider";
-
-// function App() {
-//   const [showWelcome, setShowWelcome] = useState(true);
-
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       setShowWelcome(false);
-//     }, 3000);
-
-//     return () => clearTimeout(timer);
-//   }, []);
-
-//   return (
-//     <>
-//       <Navbar />
-
-//       {/* Animate welcome message */}
-//       <AnimatePresence>
-//         {showWelcome && (
-//           <motion.div
-//             initial={{ opacity: 1, y: 0 }}
-//             animate={{ opacity: 1, y: 0 }}
-//             exit={{ opacity: 0, y: -50 }}
-//             transition={{ duration: 0.8 }}
-//             className="text-center py-2"
-//           >
-//             <h1 className="text-4xl font-bold text-gray-800">
-//               Welcome to CP PLUS
-//             </h1>
-//             <p className="mt-4 text-gray-600">Your trusted solutions provider</p>
-//           </motion.div>
-//         )}
-//       </AnimatePresence>
-
-//       {/* Smooth transition for PhotoSlider */}
-//       <motion.div
-//         initial={{ y: 50, opacity: 0 }} // Start from below
-//         animate={{ y: 0, opacity: 1 }} // Move up smoothly
-//         transition={{ duration: 1.2, ease: "easeOut", delay: showWelcome ? 0.8 : 0 }} // Delay to wait for welcome message fade-out
-//       >
-//         <PhotoSlider />
-//       </motion.div>
-//     </>
-//   );
-// }
-
-// export default App;
-
-
-
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Navbar from "../src/components/navbar";
 import PhotoSlider from "./components/slider";
+import WeOffer from "./components/weoffer";
+import StayUpdated from "./components/stayupdated";
 
 function App() {
   const [showWelcome, setShowWelcome] = useState(true);
@@ -100,7 +46,11 @@ function App() {
       >
         <PhotoSlider />
       </motion.div>
+
+      < WeOffer />
+      <StayUpdated />
     </>
+    
   );
 }
 
